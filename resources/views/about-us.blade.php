cbe53aac653cbc1a06a1503073c3210dd030c846
Don-Pier-lab <pierpaolo.sagari@gmail.com>
2026-06-11 23:36:54 +0200

    commit

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chi siamo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/chi-siamo">chi siamo</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<header>
+    <div class="container-fluid header">
+        <div class="row h-100 justify-content-around align-items-center">
+            <div class="col-6">
+                <h2 class="text-white text-color text-center">Chi siamo</h2>
+                <p class="text-white text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptas voluptates commodi doloribus architecto nostrum voluptatum quae repellat sint quos amet, non unde veritatis quam ipsa assumenda dignissimos cum illum itaque animi culpa error quasi in tenetur. Rerum inventore necessitatibus sunt impedit eligendi, pariatur reiciendis ratione. Eaque libero at aperiam.</p>
+            </div>
+             <li class="nav-item">
+          <a class="nav-link" href="/contatti">contatti</a>
+        </li>
+            <div class="col-6">
+                <img src="/media/team.jpg" alt="foto del team" class="shadow rounded">
+            </div>
+        </div>
+    </div>
+</header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
